26d22156dff57a8696bfa906160805e7ef38b426
Hauxii <haukur91@gmail.com>
2016-01-21 10:12:32 +0000

    lineto og draw virkni

--- js/main.js
+++ js/main.js
@@ -2,15 +2,63 @@ $(document).ready(function(){
 	var canvas = document.getElementById("myCanvas");
 	var context = canvas.getContext("2d");
 
+	var startx = 0;
+	var starty = 0;
+	var currx = 0;
+	var curry = 0;
+
+	var isDrawing = false;
+
 	$("#myCanvas").mousedown(function(e){
 		console.log("its alive");
-		var x = e.pageX - this.offsetLeft;
-		var y = e.pageY - this.offsetTop;
-		context.beginPath();
+		//startx = e.pageX - this.offsetLeft;
+		//starty = e.pageY - this.offsetTop;
+		
+		startx = currx;
+        starty = curry;
+        currx = e.pageX - canvas.offsetLeft;
+        curry = e.pageY - canvas.offsetTop;
+
+		isDrawing = true;
 		
 		console.log("X:" + x + " , Y: " + y);
 	});
+
+	$("#myCanvas").mousemove(function(e){
+		if(isDrawing === true){
+			//var x = e.pageX - this.offsetLeft;
+			//var y = e.pageY - this.offsetTop;
+
+			//draw virknin
+			startx = currx;
+            starty = curry;
+            currx = e.pageX - canvas.offsetLeft;
+        	curry = e.pageY - canvas.offsetTop;
+            
+            context.beginPath();
+		    context.moveTo(startx, starty);
+		    context.lineTo(currx, curry);
+		    context.strokeStyle = "black";
+		    context.lineWidth = 2;
+		    context.stroke();
+		    context.closePath();
+
+			/*lineTo virknin
+			context.clearRect(0,0,550,550);
+			context.beginPath();
+			context.moveTo(startx,starty);
+			context.lineTo(x,y);
+			context.stroke();*/
+		}
+
+	});
+
+	$("#myCanvas").mouseup(function(e){
+		isDrawing = false;
+	});
+
 });
+
 
 function set_radio($inputId){
 	$("input#" + $inputId).click();
