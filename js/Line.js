545a714099718edb444a09e586fa638de0fb42fb
kristinnhk <kristinnhk@gmail.com>
2016-01-30 15:17:36 +0000

    fixing hawks shit and line vesen from me

---
+++ js/Line.js
@@ -0,0 +1,35 @@
+var Pen = Shape.extend({
+
+	constructor: function(x, y) {
+		this.base(x, y);
+		this.startx = x;
+		this.starty = y;
+		this.currx = x;
+		this.curry = y;
+		this.points = [{x: x, y: y}];
+		console.log(this.startx + " " + this.starty + " " + this.currx + " " + this.curry);
+	},
+
+	draw: function(context, ev) {
+		//console.log("START\nX:" + this.startx + " , Y: " + this.starty + "\nCURR\nX: " + this.currx + " , Y: " + this.curry);
+		for (var i = 0; i < this.points.length; i++) {
+		    context.moveTo(this.startx, this.starty);
+		    //console.log("line from " + this.startx + " " + this.starty + " to " + this.currx + " " + this.curry);
+		    context.lineTo(this.currx, this.curry);
+		    context.strokeStyle = getshape();
+		    context.lineWidth = 2;
+		    context.stroke();
+		    context.closePath();
+		}		
+	},
+
+	drawing:function(canvas, e) {
+		this.startx = this.currx;
+        this.starty = this.curry;
+        this.currx = e.pageX - canvas.offsetLeft;
+        this.curry = e.pageY - canvas.offsetTop;
+        this.points.push({x: this.currx, y: this.curry});
+        //console.log("X:" + this.startx + " , Y: " + this.starty);
+	}
+
+});
